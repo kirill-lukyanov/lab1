c2c2b4b7fca12310dfb0465f85df36398a81f40c
kirill-lukyanov <71383135+kirill-lukyanov@users.noreply.github.com>
2020-10-01 16:10:29 +0300

    Add files via upload

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            double x, y, res;
+            int n;
+
+            Console.Write("x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Количество итераций = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            res = 0;
+            for (int i = 1, j = 0; j < n; i+=2, j++)
+            {
+                if (j % 2 == 0) res += Math.Pow(x, i + 1) / (i * (i + 2));
+                else res -= Math.Pow(y, i + 1) / (i * (i + 2));
+            }
+
+            Console.WriteLine($"res = {res}");
+            Console.ReadKey();
+        }
+
+    }
+}
